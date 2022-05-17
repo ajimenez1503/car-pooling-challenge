04b2d7596cc1b2a8c8c01bf760c8f097be2a699d
Antonio Jimenez <ajimenez@rti.com>
2022-05-17 22:59:31 +0200

    Add test

--- src/test/java/com/carpooling/CarPoolingApplicationTests.java
+++ src/test/java/com/carpooling/CarPoolingApplicationTests.java
@@ -1,13 +1,39 @@
 package com.carpooling;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.*;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CarPoolingApplicationTests {
 
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
     @Test
-    void contextLoads() {
+    public void contextLoads() throws Exception {
+        assertThat(controller).isNotNull();
+    }
+
+
+    @Test
+    public void getStatusAPI() throws Exception {
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<Object> entity = new HttpEntity<Object>(headers);
+
+        ResponseEntity<String> response = restTemplate.exchange("http://localhost:" + port + "/status", HttpMethod.GET, entity, String.class);
+
+        assertNull(response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 
 }
